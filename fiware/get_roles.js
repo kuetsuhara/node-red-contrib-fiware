9408863802d0b3f93dbd776209d2d186044ff148
Kazutoshi  Uetsuhara <kazutoshi.u@gmail.com>
2019-12-26 12:28:35 +0900

    worked get roles

--- fiware/get_roles.js
+++ fiware/get_roles.js
@@ -1,13 +1,54 @@
+const fClient = require('./fiware_client.js')
+
 module.exports = function (RED) {
   'use strict'
   function GetRolesNode(config) {
     RED.nodes.createNode(this, config)
+    this.login = RED.nodes.getNode(config.login)
+    if (!this.login) {
+      console.log('not login ??')
+      node.status({
+        fill: 'red',
+        shape: 'dot',
+        text: 'Credential error'
+      })
+      node.error('No credentials specified')
+      return
+    }
     var node = this
 
-    node.on('input', function(){
-      console.log("hoo")
-    })
+    node.on('input', function(msg){
+      var cl = new fClient(
+        this.login.server, 
+        this.login.id, 
+        this.login.password)
 
+      cl.getToken(function(err, token){
+        node.status({ fill: 'green', shape: 'dot', text: 'request...' })
+        if(err){
+          node.status({ fill: 'red', shape: 'dot', text: 'error' })
+          console.log("get token error!", err)
+        }else{
+          // GET applications
+          cl.getRoleID(token, config.appid, function(err, success){
+            if(err){
+              console.log("get aplist error!", err)
+              node.status({ fill: 'red', shape: 'dot', text: 'error' })
+              msg.payload = err
+              node.send(msg)
+            }else{
+              // console.log(success)
+              node.status({})
+              msg.payload = success
+              node.send(msg)
+            }
+          })
+        }
+      })
+    })
+    node.on('close', function() {
+      node.status({})
+    })
   }
   RED.nodes.registerType('GetRoles', GetRolesNode,)
 }
